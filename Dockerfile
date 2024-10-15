0c69ceab2c946a1bfa93e8feb0878a362a16a3b6
Kunihiko Miyanaga <miyanaga@ideamans.com>
2024-10-15 16:41:11 +0900

    ビルドスクリプトを修正

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,9 @@ FROM node:22
 ADD . /app
 WORKDIR /app
 
+RUN apt-get update
+RUN apt-get install -y build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
+
 RUN yarn install
 
 CMD ["yarn", "start"]
--- docker.sh
+++ docker.sh
@@ -7,6 +7,6 @@ DOCKERHUB_IMAGE=${DOCKERHUB_IMAGE:-"ideamans/$APP"}
 docker build -t $APP .
 
 for TAG in $VERSION latest; do
-  docker tag $LOCAL_IMAGE $DOCKERHUB_IMAGE:$TAG
+  docker tag $APP $DOCKERHUB_IMAGE:$TAG
   docker push $DOCKERHUB_IMAGE:$TAG
 done
